dab6926ecd6ec56f4b67d86e2274fedab25666ba
Ranjan Pandit <59310086+Ranjanunicode@users.noreply.github.com>
2021-10-16 20:34:26 +0530

    Add files via upload

---
+++ flag.py
@@ -0,0 +1,66 @@
+import turtle
+
+
+def flags():
+    # object of turtle module class
+    flag = turtle.Turtle()
+
+    # function for blue wheel
+
+    # flag outline
+    flag.penup()
+    flag.setposition(100, 80)
+    flag.pendown()
+    flag.backward(200)
+    flag.right(90)
+    flag.forward(50)
+    flag.left(90)
+    flag.forward(200)
+    flag.backward(200)
+    flag.right(90)
+    flag.forward(50)
+    flag.left(90)
+    flag.forward(200)
+    flag.backward(200)
+    flag.right(90)
+    flag.forward(50)
+    flag.left(90)
+    flag.forward(200)
+    flag.left(90)
+    # green shade lines
+    flag.pencolor("green")
+    i = 0
+    for i in range(17):
+        flag.left(5)
+        flag.forward(50 + i)
+        flag.backward(50+i)
+        i = i+1
+    flag.right(85)
+    # back to top
+    flag.pencolor("black")
+    flag.forward(150)
+    flag.left(90)
+    flag.forward(200)
+    flag.left(90)
+    # orange shade lines
+    flag.pencolor("orange")
+    i = 0
+    for i in range(17):
+        flag.left(5)
+        flag.forward(50 + i)
+        flag.backward(50+i)
+        i = i+1
+    flag.penup()
+    # middle blue wheel
+    flag.setposition(0, 0)
+    flag.pendown()
+    flag.pencolor("blue")
+    for i in range(26):
+        flag.forward(20)
+        flag.backward(20)
+        flag.left(14)
+
+    flag.penup()
+
+
+# flags()
